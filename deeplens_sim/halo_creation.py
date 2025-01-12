4b5ea253d1dff4eb1ea5fd0ad47cd0a986ad8e0b
lparis3 <luccaparis17@gmail.com>
2025-01-12 14:17:23 -0500

    Update halo_creation.py

--- deeplens_sim/halo_creation.py
+++ deeplens_sim/halo_creation.py
@@ -1,1 +1,258 @@
+#Construct host halo, sub halos, and field halos 
+    import pyHalo
+    import pyHalo.preset_models
+    from astropy import units as u
+    from astropy.constants import G, c, M_sun
 
+def Halo_constructor(DM_type, redshifts):
+  
+  def Host_mass(mean = 13, sigma =1.0):
+      '''Samples log of host mass from normal distribution'''
+      M_host = np.random.normal(loc = mean,scale = sigma,size =1)
+      return M_host[0]
+
+  def Host_slope(gammaL = 1.9, gammaH = 2.2):
+      '''Samples host halos log slope between lower and upper bounds from Gilman et al. 2022'''
+      return np.random.uniform(gammaL,gammaH,1)
+  
+  def CDM_constructor(zsource, zlens, M_host, Host_gamma):
+      '''This function constructs a lens (host halo, sub halo, LOS halos, and lens galaxy) under the assumption of CDM'''
+      arcsec_opening_angle = 10
+
+      #First, use Pyhalo to create subhalo realization
+      Subhalo_constructor= pyHalo.preset_models.preset_model_from_name('CDM')
+      Subhalo_realization = Subhalo_constructor(z_lens=zlens,z_source=zsource,log_m_host=M_host,cone_opening_angle_arcsec=arcsec_opening_angle) 
+
+      #Extract both lensing quantities from subhalo realization (reshifts, lensing models, lensing kwargs) and the cosmology instance from source to observer
+      Sub_model_list, Sub_redshift_array, Sub_kwargs, _ = Subhalo_realization.lensing_quantities()
+      cosmology = Subhalo_realization.astropy_instance
+
+      #Calc Einstein radius
+      def mass_to_radius(Mass,redshift_src,redshift_def):
+          M_Halo = Mass * M_sun
+          rad_to_arcsec = 206265
+
+          DL = cosmology.luminosity_distance(redshift_def).to(u.m)
+          DS = cosmology.luminosity_distance(redshift_src).to(u.m)
+          DLS = DS - DL
+
+          # Einstein radius
+          theta = np.sqrt(4 * G * M_Halo/c**2 * DLS/(DL*DS))
+
+          # Return radius in arcsecods
+          radius_arcsec = theta * rad_to_arcsec
+
+          return radius_arcsec.value
+
+      theta_E = mass_to_radius(M_host,zsource,zdeflector)
+
+
+      #Host halo
+      Host_halo = 'EPL'
+      Host_kwargs = {'theta_E':theta_E,'gamma':Host_gamma,'e1':0.4,'e2':-0.1,'center_x':0.0, 'center_y':0.0} 
+
+      #External shear
+      External_shear = 'SHEAR'
+      Shear_kwargs = {'gamma1':0.03,'gamma2':0.01,'ra_0': 0.0, 'dec_0': 0.0} 
+
+      #Macrolens
+      Macro_model_list = [Host_halo,External_shear]
+      Macro_kwargs_list = [Host_kwargs,Shear_kwargs]
+      Macro_redshift_list = [zlens,zlens]
+
+      #Combine macro-model with substructure 
+      lens_model_list = Macro_model_list + Sub_model_list
+      lens_kwargs_list = Macro_kwargs_list + Sub_kwargs
+      lens_redshift_list = Macro_redshift_list + list(Sub_redshift_array)
+
+      return lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list, arcsec_opening_angle
+
+  def Axion_constructor(zsource, zlens, M_host, Host_gamma):
+      '''This function constructs a lens (host halo, sub halo, LOS halos, and lens galaxy) under the assumption of axionic dark matter'''
+      
+      M_axion = np.random.uniform(-23,-19,1)
+      flucs_shape='ring'
+      arcsec_opening_angle = 10
+      flucs_args={'angle': 0.0, 'rmin': 0.9, 'rmax': 1.1}
+
+      #First, use Pyhalo to create subhalo realization
+      Subhalo_constructor= pyHalo.preset_models.preset_model_from_name('ULDM')
+      Subhalo_realization = Subhalo_constructor(z_lens=zlens,z_source=zsource,log10_m_uldm = M_axion ,flucs_shape=flucs_shape,flucs_args=flucs_args , log_m_host=M_host,cone_opening_angle_arcsec=arcsec_opening_angle) 
+
+      #Extract both lensing quantities from subhalo realization (reshifts, lensing models, lensing kwargs) and the cosmology instance from source to observer
+      Sub_model_list, Sub_redshift_array, Sub_kwargs, _ = Subhalo_realization.lensing_quantities()
+      cosmology = Subhalo_realization.astropy_instance
+
+      #Calc Einstein radius
+      def mass_to_radius(Mass,redshift_src,redshift_def):
+          M_Halo = Mass * M_sun
+          rad_to_arcsec = 206265
+
+          DL = cosmology.luminosity_distance(redshift_def).to(u.m)
+          DS = cosmology.luminosity_distance(redshift_src).to(u.m)
+          DLS = DS - DL
+
+          # Einstein radius
+          theta = np.sqrt(4 * G * M_Halo/c**2 * DLS/(DL*DS))
+
+          # Return radius in arcsecods
+          radius_arcsec = theta * rad_to_arcsec
+
+          return radius_arcsec.value
+
+      theta_E = mass_to_radius(M_host,zsource,zdeflector)
+
+      #Host halo
+      Host_halo = 'EPL'
+      Host_kwargs = {'theta_E':theta_E,'gamma': Host_gamma,'e1':0.4,'e2':-0.1,'center_x':0.0, 'center_y':0.0} 
+
+      #External shear
+      External_shear = 'SHEAR'
+      Shear_kwargs = {'gamma1':0.03,'gamma2':0.01,'ra_0': 0, 'dec_0': 0} 
+
+      #Macrolens
+      Macro_model_list = [Host_halo,External_shear]
+      Macro_kwargs_list = [Host_kwargs,Shear_kwargs]
+      Macro_redshift_list = [zlens,zlens]
+
+      #Combine macro-model with substructure 
+      lens_model_list = Macro_model_list + Sub_model_list
+      lens_kwargs_list = Macro_kwargs_list + Sub_kwargs
+      lens_redshift_list = Macro_redshift_list + list(Sub_redshift_array)
+
+      return lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list, arcsec_opening_angle, M_axion, flucs_shape,flucs_args
+      
+  def WDM_constructor(zsource, zlens, M_host, Host_gamma):
+      '''This function constructs a lens (host halo, sub halo, LOS halos, and lens galaxy) under the assumption of WDM'''
+      
+      log_mc = 7.0
+      arcsec_opening_angle = 10
+
+      #First, use Pyhalo to create subhalo realization
+      Subhalo_constructor= pyHalo.preset_models.preset_model_from_name('WDM')
+      Subhalo_realization = Subhalo_constructor(z_lens=zlens,z_source=zsource,log_mc=log_mc,log_m_host=M_host,cone_opening_angle_arcsec=arcsec_opening_angle) 
+
+      #Extract both lensing quantities from subhalo realization (reshifts, lensing models, lensing kwargs) and the cosmology instance from source to observer
+      Sub_model_list, Sub_redshift_array, Sub_kwargs, _ = Subhalo_realization.lensing_quantities()
+      cosmology = Subhalo_realization.astropy_instance
+
+      #Calc Einstein radius
+      def mass_to_radius(Mass,redshift_src,redshift_def):
+          M_Halo = Mass * M_sun
+          rad_to_arcsec = 206265
+
+          DL = cosmology.luminosity_distance(redshift_def).to(u.m)
+          DS = cosmology.luminosity_distance(redshift_src).to(u.m)
+          DLS = DS - DL
+
+          # Einstein radius
+          theta = np.sqrt(4 * G * M_Halo/c**2 * DLS/(DL*DS))
+
+          # Return radius in arcsecods
+          radius_arcsec = theta * rad_to_arcsec
+
+          return radius_arcsec.value
+
+      theta_E = mass_to_radius(M_host,zsource,zdeflector)
+
+      #Host Halo
+      Host_halo = 'EPL'
+      Host_kwargs = {'theta_E':theta_E,'gamma': Host_gamma,'e1':0.4,'e2':-0.1,'center_x':0.0, 'center_y':0.0} 
+
+      #External shear
+      External_shear = 'SHEAR'
+      Shear_kwargs = {'gamma1':0.03,'gamma2':0.01,'ra_0': 0, 'dec_0': 0} 
+
+      #Macrolens
+      Macro_model_list = [Host_halo,External_shear]
+      Macro_kwargs_list = [Host_kwargs,Shear_kwargs]
+      Macro_redshift_list = [zlens,zlens]
+
+      #Combine macro-model with substructure 
+      lens_model_list = Macro_model_list + Sub_model_list
+      lens_kwargs_list = Macro_kwargs_list + Sub_kwargs
+      lens_redshift_list = Macro_redshift_list + list(Sub_redshift_array)
+
+      return lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology, Macro_model_list, Macro_kwargs_list, Macro_redshift_list, arcsec_opening_angle,log_mc
+
+  def SIDM_constructor(zsource, zlens, M_host, Host_gamma):
+      '''This function constructs a lens (host halo, sub halo, LOS halos, and lens galaxy) under the assumption of SIDM'''
+      
+      mass_ranges_subhalos=[[6.0, 7.0], [7.0, 8.0], [8.0, 9.0], [9.0, 10.0]]
+      mass_ranges_field_halos=[[6.0, 7.5], [7.5, 8.5], [8.5, 10.0]]
+      probabilities_subhalos=[0.9, 0.7, 0.5, 0.2]
+      probabilities_field_halos=[0.3, 0.2, 0.1]
+      cone_opening_angle_arcsec=10
+
+
+      #First, use Pyhalo to create subhalo realization
+      Subhalo_constructor= pyHalo.preset_models.preset_model_from_name('SIDM_core_collapse')
+      Subhalo_realization = Subhalo_constructor(z_lens=zlens,z_source=zsource,mass_ranges_subhalos=mass_ranges_subhalos,mass_ranges_field_halos=mass_ranges_field_halos,probabilities_subhalos=probabilities_subhalos,probabilities_field_halos=probabilities_field_halos,log_m_host=M_host,cone_opening_angle_arcsec=cone_opening_angle_arcsec) 
+
+      #Extract both lensing quantities from subhalo realization (reshifts, lensing models, lensing kwargs) and the cosmology instance from source to observer
+      Sub_model_list, Sub_redshift_array, Sub_kwargs, _ = Subhalo_realization.lensing_quantities()
+      cosmology = Subhalo_realization.astropy_instance
+
+      #Calc Einstein radius
+      def mass_to_radius(Mass,redshift_src,redshift_def):
+          M_Halo = Mass * M_sun
+          rad_to_arcsec = 206265
+
+          DL = cosmology.luminosity_distance(redshift_def).to(u.m)
+          DS = cosmology.luminosity_distance(redshift_src).to(u.m)
+          DLS = DS - DL
+
+          # Einstein radius
+          theta = np.sqrt(4 * G * M_Halo/c**2 * DLS/(DL*DS))
+
+          # Return radius in arcsecods
+          radius_arcsec = theta * rad_to_arcsec
+
+          return radius_arcsec.value
+
+      theta_E = mass_to_radius(M_host,zsource,zdeflector)
+
+      #Host Halo
+      Host_halo = 'EPL'
+      Host_kwargs = {'theta_E':theta_E,'gamma': Host_gamma,'e1':0.4,'e2':-0.1,'center_x':0.0, 'center_y':0.0} 
+
+      #External shear
+      External_shear = 'SHEAR'
+      Shear_kwargs = {'gamma1':0.03,'gamma2':0.01,'ra_0': 0.0, 'dec_0': 0.0} 
+
+      #Macrolens
+      Macro_model_list = [Host_halo,External_shear]
+      Macro_kwargs_list = [Host_kwargs,Shear_kwargs]
+      Macro_redshift_list = [zlens,zlens]
+
+      #Combine macro-model with substructure 
+      lens_model_list = Macro_model_list + Sub_model_list
+      lens_kwargs_list = Macro_kwargs_list + Sub_kwargs
+      lens_redshift_list = Macro_redshift_list + list(Sub_redshift_array)
+
+      return lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology, Macro_model_list, Macro_kwargs_list, Macro_redshift_list, mass_ranges_subhalos, mass_ranges_field_halos, probabilities_subhalos, probabilities_field_halos, cone_opening_angle_arcsec
+
+  m_Host = Host_mass()
+  slope_Host = Host_slope()
+  zdeflector = redshifts[0]
+  zsource = redshifts[1]
+
+  
+  if DM_Type == 'CDM':
+      lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list,arcsecond_opening_angle=CDM_constructor(zsource=zsource,zlens=zdeflector,M_host=m_Host,Host_gamma=slope_Host)
+      return lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list,arcsecond_opening_angle
+
+  elif DM_Type == 'WDM':
+      lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list, log_mc, arcsecond_opening_angle =WDM_constructor(zsource=zsource,zlens=zdeflector,M_host=m_Host,Host_gamma=slope_Host)
+      return lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list, log_mc, arcsecond_opening_angle
+
+  elif DM_Type == 'SIDM':
+      lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list, mass_ranges_subhalos, mass_ranges_field_halos, probabilities_subhalos, probabilities_field_halos, arcsecond_opening_angle=SIDM_constructor(zsource=zsource,zlens=zdeflector,M_host=m_Host,Host_gamma=slope_Host)
+      return lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list, mass_ranges_subhalos, mass_ranges_field_halos, probabilities_subhalos, probabilities_field_halos, arcsecond_opening_angle
+
+  elif DM_Type == 'Axion':
+      lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list,M_axion,arcsecond_opening_angle=Axion_constructor(zsource=zsource,zlens=zdeflector,M_host=m_Host,Host_gamma=slope_Host,M_axion=M_axion),M_axion
+      return lens_model_list,lens_kwargs_list,lens_redshift_list,cosmology,Macro_model_list, Macro_kwargs_list, Macro_redshift_list,M_axion,arcsecond_opening_angle
+
+
+ 
