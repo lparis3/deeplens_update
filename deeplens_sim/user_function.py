ade74b3781b979f5e376ec1f0a9bcaa835199a18
lparis3 <luccaparis17@gmail.com>
2025-02-21 12:07:18 -0500

    Update user_function.py

--- deeplens_sim/user_function.py
+++ deeplens_sim/user_function.py
@@ -1,25 +1,25 @@
 import os
-import pickle
+import numpy as np
 from datetime import date
 from deeplens_sim.simulation import dl_sim
 
 
 def User_Inputs(DM_Type, Instrument, Sim_Number, Results_List = False):
   '''Only function user has to deal with when simulating.'''
-    results_list = []
-    os.mkdir(f'./{DM_Type}_{Instrument}_{Sim_Number}_{date.today().strftime("%x")}')
-    for i in np.linspace(1,Sim_Number, num = Sim_Number):
-      results = dl_sim(DM_Type, Instrument)
-      if Results_List == True:
-        results_list.append(results)
-      np.save(f'{DM_Type}_{Instrument}_{Sim_Number}_{date.today().strftime("%x")/{DM_Type}_{Instrument}_{i}')
-      print('Simulation {i}/{Sim_Num} Done!')
-    if not results_list:
-      print(f'All {Sim_Num} Simulations Done!')
-    else:
-      print(f'All {Sim_Num} Simulations Done!')
-      return results_list
-      
+  results_list = []
+  os.mkdir(f'./{DM_Type}_{Instrument}_{Sim_Number}_{date.today().strftime("%x")}')
+  for i in np.linspace(1,Sim_Number, num = Sim_Number):
+    results = dl_sim(DM_Type, Instrument)
+    if Results_List == True:
+      results_list.append(results)
+    np.save(f'{DM_Type}_{Instrument}_{Sim_Number}_{date.today().strftime("%x")}/{DM_Type}_{Instrument}_{i}')
+    print('Simulation {i}/{Sim_Num} Done!')
+  if not results_list:
+    print(f'All {Sim_Number} Simulations Done!')
+  else:
+    print(f'All {Sim_Number} Simulations Done!')
+    return results_list
+    
 
     
       
