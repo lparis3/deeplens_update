b2c621c01b381a3efe47853269a5a104f4164944
lparis3 <luccaparis17@gmail.com>
2025-02-26 10:34:41 -0500

    Update user_function.py

--- deeplens_sim/user_function.py
+++ deeplens_sim/user_function.py
@@ -7,7 +7,8 @@ from deeplens_sim.simulation import dl_sim
 def User_Inputs(DM_Type, Instrument, Sim_Number, Results_List = False):
   '''Only function user has to deal with when simulating.'''
   results_list = []
-  os.mkdir(f'./{DM_Type}_{Instrument}_{Sim_Number}_{date.today().strftime("%x")}')
+  Date = date.today().strftime("%x")
+  os.mkdir(f'./{DM_Type}_{Instrument}_{Sim_Number}_{Date}')
   for i in np.linspace(1,Sim_Number, num = Sim_Number):
     results = dl_sim(DM_Type, Instrument)
     if Results_List == True:
