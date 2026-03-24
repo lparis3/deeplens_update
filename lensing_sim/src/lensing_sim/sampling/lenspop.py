5ac00772b5e23b62a79f933cc825e0acc8d2940a
lparis3 <luccaparis17@gmail.com>
2026-03-24 18:53:19 -0400

    Update lenspop.py

--- lensing_sim/src/lensing_sim/sampling/lenspop.py
+++ lensing_sim/src/lensing_sim/sampling/lenspop.py
@@ -1,1 +1,20 @@
+import torch
+import numpy as np
+from lensing_sim.flows.lenspop_flow import build_joint_flow
 
+
+def load_lenspop_flow(path, device="cpu"):
+    flow = build_joint_flow(device)
+    checkpoint = torch.load(path, map_location=device)
+    flow.load_state_dict(checkpoint['flow_state'])
+    return flow, checkpoint['mean'], checkpoint['std']
+
+
+def sample_lenspop(flow, mean, std, n_samples):
+    with torch.no_grad():
+        samples = flow.sample(n_samples).cpu().numpy()
+
+    samples = (samples * std) + mean
+    samples = np.delete(samples, 2, axis=1)
+
+    return 10 ** samples
